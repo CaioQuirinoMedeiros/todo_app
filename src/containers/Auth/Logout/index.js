d2c8cfba0475d7d2f3f088fa8c0da2a739c06076
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-08 17:11:51 -0300

    SignOut feature

---
+++ src/containers/Auth/Logout/index.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+import AuthActions from "../../../store/ducks/auth";
+
+// import { Container } from './styles';
+
+const Logout = ({ signOut }) => {
+  useEffect(() => {
+    signOut();
+  }, [signOut]);
+
+  return null;
+};
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(AuthActions, dispatch);
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Logout);
