25606d0d5577b36170a59effe7e1b38c41308f47
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-09 11:56:14 -0300

    Finished verify email

--- src/containers/Auth/VerifyEmail/index.js
+++ src/containers/Auth/VerifyEmail/index.js
@@ -1,27 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
 import AuthActions from "../../../store/ducks/auth";
 
-import { Container, Form, Title, SubTitle, Button } from "../styles";
+import { Container, Form, Title, SubTitle, Button, Error } from "../styles";
 
-const VerifyEmail = ({ loading, error, verifyRequest }) => (
-  <Container>
-    <Form>
-      <Title>Verify your email</Title>
-      <SubTitle>Go to your email inbox and please verify your email.</SubTitle>
-      <Button type="button" onClick={() => verifyRequest()}>
-        {loading ? "Sending email..." : "Re-send verification email"}
-      </Button>
-      {error && <span>{error}</span>}
-    </Form>
-  </Container>
-);
+const VerifyEmail = ({ loading, message, verifyRequest, cleanUp }) => {
+  useEffect(() => {
+    return () => {
+      cleanUp();
+    };
+  }, [cleanUp]);
+
+  return (
+    <Container>
+      <Form>
+        <Title>Verify your email</Title>
+        <SubTitle>
+          Go to your email inbox and please verify your email.
+        </SubTitle>
+        <Button type="button" onClick={() => verifyRequest()}>
+          {loading ? "Sending email..." : "Re-send verification email"}
+        </Button>
+        {message.content && (
+          <Error center type={message.type}>
+            {message.content}
+          </Error>
+        )}
+      </Form>
+    </Container>
+  );
+};
 
 const mapStateToProps = ({ auth }) => ({
   loading: auth.verifyEmail.loading,
-  error: auth.verifyEmail.error
+  message: auth.verifyEmail.message
 });
 
 const mapDispatchToProps = dispatch =>
