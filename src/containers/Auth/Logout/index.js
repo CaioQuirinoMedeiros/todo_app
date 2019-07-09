49a61149b8a8ea1e80289c3f463231e4f7b28216
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-09 20:33:43 -0300

    Removing unused imports

--- src/containers/Auth/Logout/index.js
+++ src/containers/Auth/Logout/index.js
@@ -1,7 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { Redirect } from "react-router-dom";
 
 import AuthActions from "../../../store/ducks/auth";
 
