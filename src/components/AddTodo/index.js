35840c85676bf1731c98c53e79cc48c5236ed981
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-10 20:43:38 -0300

    Add todo component

---
+++ src/components/AddTodo/index.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import { Formik, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+import TodosActions from "../../store/ducks/todos";
+
+import Button from "../../utils/button";
+import Modal from "../Modal";
+
+import {
+  Form,
+  Input,
+  ErrorWrapper,
+  Error,
+  Title,
+  SubTitle
+} from "../../containers/Auth/styles";
+
+const todoSchema = Yup.object().shape({
+  todo: Yup.string()
+    .required("The todo is required")
+    .min(3, "Too short")
+});
+
+class AddTodo extends Component {
+  componentWillUnmount() {
+    const { cleanUp } = this.props;
+    cleanUp();
+  }
+
+  render() {
+    const { addTodoRequest, loading, error } = this.props;
+
+    return (
+      <Modal closeModal={this.props.close}>
+        <Formik
+          initialValues={{ todo: "" }}
+          validationSchema={todoSchema}
+          onSubmit={({ todo }, { setSubmitting }) => {
+            addTodoRequest(todo);
+            setSubmitting(false);
+          }}
+        >
+          {({ isSubmitting, isValid }) => (
+            <Form>
+              <Title>Add new todo</Title>
+              <SubTitle>Type your todo and press add</SubTitle>
+
+              <Input type="text" name="todo" placeholder="New todo" />
+              <ErrorWrapper>
+                <ErrorMessage name="todo" component={Error} />
+              </ErrorWrapper>
+
+              <Button disabled={!isValid || loading} type="submit">
+                {loading ? "Adding todo..." : "Add"}
+              </Button>
+              <Button type="submit" red onClick={() => this.props.close()}>
+                Cancel
+              </Button>
+
+              <ErrorWrapper>
+                {error && <Error center>{error}</Error>}
+              </ErrorWrapper>
+            </Form>
+          )}
+        </Formik>
+      </Modal>
+    );
+  }
+}
+
+const mapStateToProps = ({ todos }) => ({
+  loading: todos.addTodo.loading,
+  error: todos.addTodo.error
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(TodosActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AddTodo);
