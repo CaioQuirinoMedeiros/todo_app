7da129493b14b43d74632a085cebda9582ab5e98
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-10 20:43:29 -0300

    Todo component

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,86 +1,97 @@
 import React, { Component } from "react";
-import { Formik, ErrorMessage } from "formik";
-import * as Yup from "yup";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
+import Confirmation from "../Confirmation";
+
 import TodosActions from "../../store/ducks/todos";
 
-import Button from "../../utils/button";
-import Modal from "../Modal";
-
-import {
-  Form,
-  Input,
-  ErrorWrapper,
-  Error,
-  Title,
-  SubTitle
-} from "../../containers/Auth/styles";
-
-const todoSchema = Yup.object().shape({
-  todo: Yup.string()
-    .required("The todo is required")
-    .min(3, "Too short")
-});
+import { TodoCard, CheckInput, TodoText, DeleteButton, Icon } from "./styles";
 
 class Todo extends Component {
+  state = {
+    deleteConfirmation: false
+  };
+
   componentDidMount() {
-    const { cleanUp } = this.props;
-    cleanUp();
+    const { todo } = this.props;
+
+    const textInput = document.getElementById(`text-${todo.id}`);
+
+    textInput.addEventListener("blur", this.editTodo);
   }
 
+  componentWillUnmount() {
+    const { todo } = this.props;
+    const textInput = document.getElementById(`text-${todo.id}`);
+
+    textInput.removeEventListener("blur", this.editTodo);
+  }
+
+  inputChange = e => {
+    const { todoInputChange, todo } = this.props;
+
+    todoInputChange(todo.id, e.target.textContent);
+  };
+
+  editTodo = e => {
+    const { editTodoRequest, todo } = this.props;
+
+    editTodoRequest({ ...todo, todo: e.target.textContent });
+  };
+
+  toggleTodoDone = e => {
+    const { editTodoRequest, todo } = this.props;
+
+    editTodoRequest({ ...todo, done: e.target.checked });
+  };
+
+  removeTodo = () => {
+    const { removeTodoRequest, todo } = this.props;
+
+    removeTodoRequest(todo.id);
+  };
+
   render() {
-    const { addTodoRequest, loading, error } = this.props;
+    const { todo } = this.props;
+    const { deleteConfirmation } = this.state;
 
     return (
-      <Modal closeModal={this.props.close}>
-        <Formik
-          initialValues={{ todo: "" }}
-          validationSchema={todoSchema}
-          onSubmit={({ todo }, { setSubmitting }) => {
-            console.log(todo);
-            addTodoRequest(todo);
-            setSubmitting(false);
-          }}
+      <TodoCard done={todo.done}>
+        <CheckInput>
+          <input
+            type="checkbox"
+            checked={todo.done}
+            onChange={this.toggleTodoDone}
+          />
+          <span className="checkmark" />
+        </CheckInput>
+        <TodoText contentEditable={!todo.done} id={`text-${todo.id}`}>
+          {todo.todo}
+        </TodoText>
+        <DeleteButton
+          type="button"
+          onClick={() => this.setState({ deleteConfirmation: true })}
         >
-          {({ isSubmitting, isValid }) => (
-            <Form>
-              <Title>Add new todo</Title>
-              <SubTitle>Type your todo and press add</SubTitle>
+          <Icon />
+        </DeleteButton>
 
-              <Input type="text" name="todo" placeholder="New todo" />
-              <ErrorWrapper>
-                <ErrorMessage name="todo" component={Error} />
-              </ErrorWrapper>
-
-              <Button disabled={!isValid || loading} type="submit">
-                {loading ? "Adding todo..." : "Add"}
-              </Button>
-              <Button type="submit" red onClick={() => this.props.close()}>
-                Cancel
-              </Button>
-
-              <ErrorWrapper>
-                {error && <Error center>{error}</Error>}
-              </ErrorWrapper>
-            </Form>
-          )}
-        </Formik>
-      </Modal>
+        {deleteConfirmation && (
+          <Confirmation
+            confirm={this.removeTodo}
+            close={() => this.setState({ deleteConfirmation: false })}
+            message="This action cannot be undone"
+          />
+        )}
+      </TodoCard>
     );
   }
 }
-
-const mapStateToProps = ({ todos }) => ({
-  loading: todos.loading,
-  error: todos.error
-});
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(TodosActions, dispatch);
 
 export default connect(
-  mapStateToProps,
+  null,
   mapDispatchToProps
 )(Todo);
---
+++ src/components/Todo/styles.js
@@ -0,0 +1,130 @@
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+export const TodoCard = styled.div`
+  width: 100%;
+  max-width: 80rem;
+  padding: 2rem;
+  margin: 0.5rem 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  background: ${props =>
+    props.done ? "rgba(47, 130, 184, 0.4)" : "var(--color-mainLighter)"};
+  border-radius: 5px;
+  transition: all 0.2s;
+`;
+
+export const TodoText = styled.div`
+  flex: 1;
+  min-height: 1.8rem;
+  word-break: break-word;
+  border-radius: 5px;
+  padding: 1rem;
+  margin: 0 1rem;
+
+  outline: none;
+  font-size: 1.2rem;
+  border: 1px solid transparent;
+  color: ${props =>
+    props.contentEditable ? "var(--color-white)" : "rgba(255,255,255,0.5)"};
+  cursor: ${props => (props.contentEditable ? "pointer" : "default")};
+  transition: all 0.2s;
+
+  ${props =>
+    props.contentEditable
+      ? `&:hover {
+    background: rgba(0, 0, 0, 0.05);
+    border: 1px solid rgba(0, 0, 0, 0.1);
+  }`
+      : ""}
+
+  &:focus {
+    cursor: text;
+    background: rgba(0, 0, 0, 0.05);
+    border: 1px solid rgba(0, 0, 0, 0.1);
+  }
+`;
+
+export const DeleteButton = styled.button`
+  background: none;
+`;
+
+export const Icon = styled(FontAwesomeIcon).attrs({
+  icon: faTrash,
+  size: "lg"
+})`
+  color: var(--color-mainDark);
+  opacity: 0.85;
+  transition: all 0.2s;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+export const CheckInput = styled.label`
+  display: block;
+  position: relative;
+  padding-left: 35px;
+  margin-bottom: 12px;
+  cursor: pointer;
+  font-size: 22px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
+  input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 0;
+    width: 0;
+  }
+
+  .checkmark {
+    position: absolute;
+    top: 0;
+    left: 0;
+    transform: translateY(-25%);
+    height: 2rem;
+    width: 2rem;
+    border-radius: 1rem;
+    background-color: #eee;
+
+    transition: all 0.2s;
+  }
+
+  &:hover input ~ .checkmark {
+    background-color: #ccc;
+  }
+
+  & input:checked ~ .checkmark {
+    background-color: #ddd;
+  }
+
+  .checkmark::after {
+    content: "";
+    position: absolute;
+    display: none;
+  }
+
+  & input:checked ~ .checkmark::after {
+    display: block;
+  }
+
+  & .checkmark::after {
+    left: 0.65rem;
+    top: 0.2rem;
+    width: 0.5rem;
+    height: 1.2rem;
+    border: solid var(--color-mainDark);
+    border-width: 0 3px 3px 0;
+    -webkit-transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    transform: rotate(45deg);
+  }
+`;
