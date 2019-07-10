2c3dcb4b44c0d3a70614fa4b6e831afbeeb53d5f
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-10 20:42:13 -0300

    Registering todo sagas

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -12,7 +12,7 @@ import {
 } from "./auth";
 
 import { TodosTypes } from "../ducks/todos";
-import { addTodo } from "./todos";
+import { addTodo, getTodos, editTodo, removeTodo } from "./todos";
 
 export default function* rootSaga(context) {
   yield all([
@@ -24,6 +24,9 @@ export default function* rootSaga(context) {
     takeLatest(AuthTypes.PROFILE_EDIT_REQUEST, editProfile, context),
     takeLatest(AuthTypes.DELETE_ACCOUNT_REQUEST, deleteAccount, context),
 
-    takeLatest(TodosTypes.ADD_TODO_REQUEST, addTodo, context)
+    takeLatest(TodosTypes.ADD_TODO_REQUEST, addTodo, context),
+    takeLatest(TodosTypes.GET_TODOS_REQUEST, getTodos, context),
+    takeLatest(TodosTypes.EDIT_TODO_REQUEST, editTodo, context),
+    takeLatest(TodosTypes.REMOVE_TODO_REQUEST, removeTodo, context)
   ]);
 }
