60b5b8778f53a72cc16a4f2b4a749b8dbb67cdb3
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-10 20:43:49 -0300

    More option to loader component

--- src/components/Loader/styles.js
+++ src/components/Loader/styles.js
@@ -21,10 +21,11 @@ export const LoaderComponent = styled.div`
     width: 51px;
     height: 51px;
     margin: 6px;
-    border: 6px solid #003459;
+    border: 6px solid ${props => (props.color ? props.color : "#003459")};
     border-radius: 50%;
     animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-    border-color: #003459 transparent transparent transparent;
+    border-color: ${props => (props.color ? props.color : "#003459")}
+      transparent transparent transparent;
   }
   div:nth-child(1) {
     animation-delay: -0.45s;
